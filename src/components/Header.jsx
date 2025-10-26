46496929d583930d657c27b8a449ebc3a7dbd181
AnLaxina <anilovlaxina@gmail.com>
2025-10-26 13:54:38 -0700

    feat(header): add scores to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,10 @@ export default function Header({text = "Chicken!"}) {
     return (
         <header>
             <h1>{text}</h1>
+            <div className="score-container">
+                <p>Score: 0</p>
+                <p>Best score: 0</p>
+            </div>
         </header>
     )
 }
