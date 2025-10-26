dd013d314b2e54df06a82daa3990a7b2aea5b2ed
AnLaxina <anilovlaxina@gmail.com>
2025-10-26 13:26:28 -0700

    feat: add proper spacing in `App`

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import Header from "./components/Header.jsx";
+
 import "./styles/reset.css";
+import "./styles/app.css";
+import "./styles/font.css";
+import "./styles/colour.css";
+import "./styles/spacing.css";
+
 
 function App() {
     return (
