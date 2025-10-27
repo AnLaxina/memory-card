b8617c38590607d027fe38a983ba75c0a959f692
AnLaxina <anilovlaxina@gmail.com>
2025-10-27 12:03:25 -0700

    feat(memorycard): add `getImage` function

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -6,8 +6,19 @@ export default function MemoryCard({name = "Pokémon Name"}) {
         width: "64px",
         height: "64px"
     }
+
+    async function getImage(pokemonName = "pikachu") {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
+            const data = await response.json();
+            return data["sprites"]["front_default"];
+        } catch (error) {
+            console.error("Can't fetch the image! Error: ", error)
+        }
+    }
+
     return (
-        <div className="memory-card">
+        <div className="memory-card" onClick={() => getImage()}>
             <img alt="Vite Logo" src="/vite.svg" style={bigLogo}/>
             {name}
         </div>
