cf56159ab829efd8a3deaa8366006746c9b04d7d
AnLaxina <anilovlaxina@gmail.com>
2025-10-26 19:19:18 -0700

    feat(memorycard): add `MemoryCard` component its basic styling

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,10 +1,12 @@
+import MemoryCard from "./MemoryCard.jsx";
+
 import "../styles/gameboard.css";
 
 export default function Gameboard() {
     return (
         <main>
             <div className="gameboard">
-
+                <MemoryCard/>
             </div>
         </main>
     )
---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+import "../styles/memorycard.css";
+
+export default function MemoryCard({name = "Pokémon Name"}) {
+    // This is just a placeholder for now, will delete the style once I get the PokeAPI working
+    const bigLogo = {
+        width: "64px",
+        height: "64px"
+    }
+    return (
+        <div className="memory-card">
+            <img alt="Vite Logo" src="/vite.svg" style={bigLogo}/>
+            <p>{name}</p>
+        </div>
+    )
+}
