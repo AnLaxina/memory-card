3c0912ab5bb9897184da6957048520854048688a
AnLaxina <anilovlaxina@gmail.com>
2025-10-27 12:35:31 -0700

    feat(memorycard): add `getImage` to a `useEffect` hook

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,25 +1,28 @@
 import "../styles/memorycard.css";
+import {useEffect, useState} from "react";
 
 export default function MemoryCard({name = "Pokémon Name"}) {
-    // This is just a placeholder for now, will delete the style once I get the PokeAPI working
-    const bigLogo = {
-        width: "64px",
-        height: "64px"
-    }
 
-    async function getImage(pokemonName = "pikachu") {
-        try {
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
-            const data = await response.json();
-            return data["sprites"]["front_default"];
-        } catch (error) {
-            console.error("Can't fetch the image! Error: ", error)
+    const [spriteUrl, setSpriteUrl] = useState("/vite.svg");
+
+    useEffect(() => {
+        async function getImage(pokemonName = "pikachu") {
+            try {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
+                const data = await response.json();
+                setSpriteUrl(data["sprites"]["front_default"])
+
+            } catch (error) {
+                console.error("Can't fetch the image! Error: ", error)
+            }
         }
-    }
+
+        getImage(name);
+    }, [name])
 
     return (
-        <div className="memory-card" onClick={() => getImage()}>
-            <img alt="Vite Logo" src="/vite.svg" style={bigLogo}/>
+        <div className="memory-card">
+            <img alt={`${name}, Pokemon`} src={spriteUrl}/>
             {name}
         </div>
     )
